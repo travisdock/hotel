fce8ffd8792e20956016bc83e9c18f033557c8c5
Travis D <travisdock@gmail.com>
2023-03-14 05:53:32 -0600

    Start dockerizing

---
+++ bin/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if bundle check
+then
+  echo "Bundle installing..."
+  bundle install --jobs=$(nproc) > ${BUNDLE_PATH}/bundle_install
+  tail -n 2 ${BUNDLE_PATH}/bundle_install
+else
+  bundle install --jobs=$(nproc)
+fi
+
+bundle exec rails db:prepare
+
+exec "$@"
---
+++ bin/start.sh
@@ -0,0 +1,3 @@
+rm -f tmp/pids/server.pid
+
+bin/rails server -p 3000 -b 0.0.0.0
