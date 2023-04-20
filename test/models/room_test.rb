ea4bb8a7d856817257a1da39b9313da07a9ebdd2
Travis D <travisdock@gmail.com>
2023-04-20 05:17:15 -0600

    Change wording on test

--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -5,7 +5,7 @@ class RoomTest < ActiveSupport::TestCase
     room_1 = Room.create(room_number: "101", room_type: "single queen")
     room_2 = Room.create(room_number: "102", room_type: "single queen")
     room_3 = Room.create(room_number: "103", room_type: "single queen")
-    booking_1 = Reservation.create(room_id: room_1.id, check_in: Date.today, check_out: Date.today + 1)
+    reservation_1 = Reservation.create(room_id: room_1.id, check_in: Date.today, check_out: Date.today + 1)
     available_rooms = Room.available_on(Date.today, Date.today + 1)
     assert_includes available_rooms, room_2
     assert_includes available_rooms, room_3
