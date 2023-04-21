03d5d9f8495b02f8f022d78b11b60b327c316252
Travis D <travisdock@gmail.com>
2023-04-21 05:51:49 -0600

    Fix tests

--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -2,9 +2,9 @@ require "test_helper"
 
 class RoomTest < ActiveSupport::TestCase
   test "should find available rooms" do
-    room_1 = Room.create(room_number: "101", room_type: "single queen")
-    room_2 = Room.create(room_number: "102", room_type: "single queen")
-    room_3 = Room.create(room_number: "103", room_type: "single queen")
+    room_1 = Room.create(name: "test")
+    room_2 = Room.create(name: "test1")
+    room_3 = Room.create(name: "test2")
     reservation_1 = Reservation.create(room_id: room_1.id, check_in: Date.today, check_out: Date.today + 1)
     available_rooms = Room.available_on(Date.today, Date.today + 1)
     assert_includes available_rooms, room_2
