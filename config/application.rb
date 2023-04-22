48840fb27985f3e75745abc67f98cf33a1b6fb80
Travis D <travisdock@gmail.com>
2023-04-22 06:15:35 -0600

    Remove Type model, it is a rails reserved word

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module Hotel
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_record.timestamped_migrations = false
   end
 end
