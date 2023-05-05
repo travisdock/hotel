f4f7056d484a7ac29ef521b4bcbb789dc45853e5
Travis D <travisdock@gmail.com>
2023-05-04 21:16:42 -0600

    Add ActiveStorage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,6 +54,9 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
 
+  # Store files locally.
+  config.active_storage.service = :local
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,6 +27,9 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
+  # Store uploaded files on the local file system in a temporary directory.
+  config.active_storage.service = :test
+
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
